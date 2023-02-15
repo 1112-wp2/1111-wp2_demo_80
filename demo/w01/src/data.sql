3548a828c493ac2b998f38dd23a6ebdb846d3c09
justin40715 <justin40715@gmail.com>
2023-02-15 21:27:40 +0800

    W01-P3: connect supabase from pgAdmin, and add 4 more data

---
+++ demo/w01/src/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO cart_80 (id,title,price,img,amount)
+VALUES 
+(5,'Samsung Galaxy A53',499.99,'https://wjviuyuwtkixlajqlpbk.supabase.co/storage/v1/object/public/demo-80/cart-80/galaxy-s8.png',1),
+(6,'google pixel 7',699.99,'https://wjviuyuwtkixlajqlpbk.supabase.co/storage/v1/object/public/demo-80/cart-80/Google-pixel.png',1),
+(7,'Xiaomi 13',899.99,'https://wjviuyuwtkixlajqlpbk.supabase.co/storage/v1/object/public/demo-80/cart-80/Xiaomi-note2.png',1),
+(8,'Samsung Galaxy S23',999.99,'https://wjviuyuwtkixlajqlpbk.supabase.co/storage/v1/object/public/demo-80/cart-80/Samsung-GalaxyS7.png',1)
