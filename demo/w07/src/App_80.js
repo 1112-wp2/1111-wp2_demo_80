f40e0fb6b2e42bb1aa2dca9284162c432b824d3b
justin40715 <justin40715@gmail.com>
2023-03-29 19:44:43 +0800

    W07-P1: convert global blogs css to a styled component

---
+++ demo/w07/src/App_80.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routers, Route, Routes } from 'react-router-dom';
+
+import BlogsStaticPage_80 from './pages/BlogsStaticPage_80';
+import HomePage_80 from './pages/HomePage_80';
+import BlogLocalJsonPage_80 from './pages/BlogLocalJsonPage_80';
+import BlogNodeServerPage_80 from './pages/BlogNodeServerPage_80';
+import BlogSupabasePage_80 from './pages/BlogSupabasePage_80';
+
+function App_80() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/card_80' element={<BlogsStaticPage_80 />} />
+        <Route path='/card2_local_80' element={<BlogLocalJsonPage_80 />} />
+        <Route path='/card2_node_80' element={<BlogNodeServerPage_80 />} />
+        <Route path='/supabase_80' element={<BlogSupabasePage_80 />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App_80;
