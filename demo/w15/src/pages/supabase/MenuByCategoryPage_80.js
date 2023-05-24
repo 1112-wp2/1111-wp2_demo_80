71fe3067c62bd61057881ba4d4127694cc6a0cf1
justin40715 <justin40715@gmail.com>
2023-05-24 20:19:49 +0800

    W15-P2: rewrite MenuByCategoryPage_80.js, can handle filter buttons

--- demo/w15/src/pages/supabase/MenuByCategoryPage_80.js
+++ demo/w15/src/pages/supabase/MenuByCategoryPage_80.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-
+import { useNavigate, useParams} from 'react-router-dom';
 const base_url = `https://wjviuyuwtkixlajqlpbk.supabase.co/rest/v1/menu_80?select=*`;
 
 let url = `${base_url}`
@@ -14,26 +14,32 @@ const options = {
 
 const MenuByCategoryPage_80 = () => {
   const [products, setProducts] = useState([]);
+  const [category, setCategory] = useState('all');
+  const params = useParams();
+  console.log('params category', params.category);
 
-  const changeFilter = (filter) => {
-    if(filter === 'all'){
-      url = `${base_url}`;
-    } else {
-      url = `${base_url}&category=eq.${filter}`;
-    }
-    getMenuData_80(filter);
+  const navigate = useNavigate();
+
+  const changeFilter = (category) => {
+    console.log('category', category);
+    setCategory(category);
   }
 
-  const getMenuData_80 = async(filter = 'all') => {
-      const response = await fetch(url, options);
+  const getMenuDataByCategory_80 = async() => {
+    if(params.category === 'all'){
+      url = `${base_url}`;
+    } else {
+      url = `${base_url}&category=eq.${params.category}`;
+    }
+    const response = await fetch(url, options);
       const data = await response.json();
-      console.log(`${filter} data`, data);
+      console.log('menu data', data);
       setProducts(data);
   }
 
   useEffect(()=> {
     getMenuDataByCategory_80();
-  }, []);
+  }, [category]);
   return (
     <section className="menu">
           <div className="title">
@@ -42,19 +48,19 @@ const MenuByCategoryPage_80 = () => {
             <div className="underline"></div>
           </div>
           <div className="btn-container">
-            <button type="button" className="filter-btn" data-id="all" onClick={()=> changeFilter()}>
+            <button type="button" className="filter-btn" data-id="all" onClick={()=> {changeFilter('all'); return navigate('/supa_menu_80/all');}}>
               all
             </button>
-            <button type="button" className="filter-btn" data-id="breakfast" onClick={()=>  changeFilter('breakfast')}>
+            <button type="button" className="filter-btn" data-id="breakfast" onClick={()=>  {changeFilter('breakfast'); return navigate('/supa_menu_80/breakfast');}}>
               breakfast
               </button>
-              <button type="button" className="filter-btn" data-id="lunch"  onClick={()=> changeFilter('lunch')}>
+              <button type="button" className="filter-btn" data-id="lunch"  onClick={()=> {changeFilter('lunch'); return navigate('/supa_menu_80/lunch');}}>
               lunch
               </button>
-              <button type="button" className="filter-btn" data-id="dessert"  onClick={()=> changeFilter('dessert')}>
+              <button type="button" className="filter-btn" data-id="dessert"  onClick={()=> {changeFilter('dessert'); return navigate('/supa_menu_80/dessert');}}>
               dessert
               </button>
-              <button type="button" className="filter-btn" data-id="shakes"  onClick={()=> changeFilter('shakes')}>
+              <button type="button" className="filter-btn" data-id="shakes"  onClick={()=> {changeFilter('shakes'); return navigate('/supa_menu_80/shakes');}}>
               shakes
             </button>
           </div>
